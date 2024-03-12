ca1d4d0b03a4ea2c86cfc0ffa669b68238a8b3d2
23123 <2312361148@qq.com>
2024-03-12 21:24:40 +0800

    第一次测试提交

---
+++ src/main/java/com/mychatroom/MyChatRoomApplication.java
@@ -0,0 +1,13 @@
+package com.mychatroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyChatRoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyChatRoomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mychatroom/MyChatRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mychatroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyChatRoomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
